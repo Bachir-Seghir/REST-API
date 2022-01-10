7534ab8c1dce5263b10c047fdfe4a3cf87ca9c24
MacBooK Air <macbookair@MacBooKs-MacBook-Air.local>
2022-01-10 22:45:04 +0100

    first commit

---
+++ client-payment/src/Success.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Success() {
+    return (
+        <div style={{height: '100vh',display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <h1 style={{ backgroundColor: 'yellow', padding: 15}}>
+                Successfull !
+            </h1>
+        </div>
+    )
+}
+
+export default Success
